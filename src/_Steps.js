0e63da7388d4f353f5fe14a85c40b2f6d6eebfa8
Muhammad Enamul Haque <enahaque1164@gmail.com>
2022-09-05 20:38:23 +0600

    first commit

---
+++ src/_Steps.js
@@ -0,0 +1,8 @@
+/*
+1.project create;
+2.Register app;
+3.yarn add firebase;
+4.Firebase configuration into firebase.init.js and export default app;
+5.yarn add react-router-dom@6;
+6.
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAMAcjxRrc4_uO1ZlA9cYGe07q1Z3pZxDA",
+  authDomain: "router-firebase-integrat-3651d.firebaseapp.com",
+  projectId: "router-firebase-integrat-3651d",
+  storageBucket: "router-firebase-integrat-3651d.appspot.com",
+  messagingSenderId: "509567431778",
+  appId: "1:509567431778:web:4e44952a1efeedcd980d3b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
